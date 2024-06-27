48d0256462dbab7f37ee3d0641515a1f32860f50
fatima <162475613+fatimaRiaz531@users.noreply.github.com>
2024-06-27 16:46:59 +0500

    primitives and object

---
+++ object.js
@@ -0,0 +1,18 @@
+//  primitives and objects in javascript
+// nn  bb   ss u 
+var a = null;
+var b = 123;
+var c = true; // can also be false
+var d = BigInt("123") + BigInt("5");
+var e = "fatima";
+var f = Symbol("I  am a nice symbol");
+var g = undefined;
+console.log(a, b, c, d, e, f, g);
+console.log(typeof e);
+// objects in JS
+var item = {
+    "fatima": true,
+    "aisha": false,
+    "ahsan": 123,
+};
+console.log((item["aisha"]));
---
+++ object.ts
@@ -0,0 +1,27 @@
+//  primitives and objects in javascript
+
+// nn  bb   ss u 
+
+let a = null;
+let b = 123;
+let c= true; // can also be false
+let d = BigInt("123")+BigInt("5");
+let e = "fatima";
+let f = Symbol("I  am a nice symbol")
+let g = undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof e)
+
+// objects in JS
+
+const item = {
+    "fatima": true,
+    "aisha": false,
+    "ahsan": 123,
+}
+console.log((item["aisha"]));
+
+
+
+
+
